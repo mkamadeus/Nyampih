64465fe9458ff970c3348f3dda09e381fc68b093
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-02-23 21:52:22 +0700

    Prepping stuff and added font loader

--- App.tsx
+++ App.tsx
@@ -1,15 +1,40 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import UserHomeScreen from './src/screens/user/UserHomeScreen';
 
-const navigator = createStackNavigator(
-  {
-    UserHome: UserHomeScreen,
-  },
-  {
-    initialRouteName: 'UserHome',
-    headerMode: 'none'
-  }
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(navigator);
+const fetchFont = () => {
+  return Font.loadAsync({
+    'product-sans-regular' : require('./assets/fonts/ProductSans-Regular.ttf'),
+    'product-sans-medium' : require('./assets/fonts/ProductSans-Medium.ttf'),
+    'product-sans-bold' : require('./assets/fonts/ProductSans-Bold.ttf'),
+  })
+}
+
+export default function App() {
+  const [dataLoaded, setDataLoaded] = React.useState(false);
+
+  if(!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={()=> setDataLoaded(true)}
+      />
+    );
+  }
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={UserHomeScreen} 
+          options = {{title:'My Home'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
--- src/screens/user/UserHomeScreen.tsx
+++ src/screens/user/UserHomeScreen.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet,  StatusBar} from 'react-native';
+import Constants from 'expo-constants';
+
 
 export default class UserHomeScreen extends React.Component
 {
 
   render() {
+
     return (
-      <View style={styles.container}>
-        <View style={styles.subcontainer}>
-          <Text>Pizza 1</Text>
-        </View>
-        <View style={styles.subcontainer}>
-          <Text>Pizza 1</Text>
-        </View>
-        <View style={styles.subcontainer}>
-          <Text>Pizza 1</Text>
+      <View style={{flex:1}}>
+        <View style={styles.container}>
+          <View style={styles.subcontainer}>
+            <Text style={{fontFamily: 'product-sans-bold',fontSize:72}}>Pizza 1</Text>
+          </View>
+          <View style={styles.subcontainer}>
+            <Text>Pizza 1</Text>
+          </View>
+          <View style={styles.subcontainer}>
+            <Text>Pizza 1</Text>
+          </View>
         </View>
       </View>
     );
@@ -34,7 +39,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'orange',
-    margin:50,
     width: '80%'
   }
 })
